bf2e5df142060ff9364f711bdee655b8e136c324
zero-coolio <156119062+zero-coolio@users.noreply.github.com>
2025-10-15 06:54:30 -0400

    Remove deprecated fe2 folder

--- fe2/src/components/ui/input.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import cn from 'classnames'
-export const Input = React.forwardRef(({ className='', ...props }, ref) => {
-  return <input ref={ref} className={cn('w-full rounded-md border border-slate-300 bg-white px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-slate-400', className)} {...props} />
-})
-Input.displayName = 'Input'
--- fe2/src/components/ui/label.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-export function Label({ className='', ...props }) {
-  return <label className={`text-sm ${className}`} {...props} />
-}
